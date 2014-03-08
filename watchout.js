a7f8771e7cdde65ce8c509067d8f538fdc03668b
Allegra <athomp@hawaii.edu>
2014-03-07 20:12:38 -0800

    GAME DONE!implemented high score, current score, and collisions count

--- watchout.js
+++ watchout.js
@@ -28,16 +28,8 @@ svg.selectAll('circle').data(d3.range(10))
 
 var drag = d3.behavior.drag()
   .on("drag", function(d){
-
-
-
     d.x += d3.event.dx;
     d.y += d3.event.dy;
-    console.log("d3.event.dx: ", d3.event.dx, " new d.x: ", d.x);
-    console.log("d3.event.dy: ", d3.event.dy, " new d.y: ", d.y);
-    // d3.select(this).attr("transform", function(d,i){
-    //   return "translate(" + [ d.x , d.y] + ")";
-    // });
     d3.select(this).attr("cx", function(d){
       return d.x;
     });
@@ -77,6 +69,8 @@ setInterval( function () {
 var checkDistance = function(x1, y1, x2, y2) {
   return Math.sqrt((Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)));
 };
+
+
 var checkCollision = function() {
   var player = d3.select('.goodCircle');
   var enemies = d3.selectAll('.badCircle');
@@ -84,20 +78,31 @@ var checkCollision = function() {
     var enemyX = (d3.select(this).attr('cx'));
     var enemyY = (d3.select(this).attr('cy'));
     var enemyR = d3.select(this).attr('r');
-//    console.log("enemy: x:", enemyX, " y:", enemyY, " r:", enemyR);
     var playerX = player.attr('cx');
     var playerY = player.attr('cy');
     var playerR = player.attr('r');
-    // console.log("player: x:", playerX, " y:", playerY, " r:", playerR);
-    // console.log("checkDistance: ", checkDistance(playerX, playerY, enemyX, enemyY));
-    // console.log('distance: ', (+enemyR + +playerR));
+
+    d3.select('.current span').text(1 + +d3.select('.current span').text());
+
     if(checkDistance(playerX, playerY, enemyX, enemyY) < (+enemyR + +playerR)) {
-      console.log("COLLISION");
-    }
+      d3.select('.collisions span').text(1 + +d3.select('.collisions span').text());
+      //set high score
+      if ( +d3.select('.high span').text() < +d3.select('.current span').text() ) {
+        d3.select('.high span').text(d3.select('.current span').text());
+      }
+      //set score to zero
+      d3.select('.current span').text(0);
+
+    } 
   });
 };
 
+// collision clock
 setInterval(function() {
   checkCollision();
 }, 100
 );
+
+// score increment
+setInterval 
+
