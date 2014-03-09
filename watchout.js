2c3d8a2206fd2ff59eed29e63bf056450d186887
Allegra <athomp@hawaii.edu>
2014-03-08 17:14:33 -0800

    enemies move at different times

--- watchout.js
+++ watchout.js
@@ -6,33 +6,36 @@ var w = window.innerWidth, h = window.innerHeight;
 
 
 
-svg.append("defs").append("pattern")
-      .attr("id", "image")
-      .attr("x", "0")
-      .attr("y", "0")
-      .attr("height", "40")
-      .attr("width", "40")
-    .append("image")
-      .attr("x", "0")
-      .attr("y", "0")
-      .attr("height", "40")
-      .attr("width", "40")
-      .attr("xlink:href", "http://img18.imageshack.us/img18/7293/shuriken2.png")
-      
-    ;
+// svg.append("defs").append("pattern")
+//       .attr("id", "image")
+//       .attr("x", "0")
+//       .attr("y", "0")
+//       .attr("height", "40")
+//       .attr("width", "40")
+//     .append("image")
+//       .attr("x", "0")
+//       .attr("y", "0")
+//       .attr("height", "40")
+//       .attr("width", "40")
+//       .attr("xlink:href", "http://guerreromarcial.webcindario.com/Imagenes/thick_ninja_shuriken_540.jpg")
+//     ;
+
+var radius = 20;
 
 svg.attr({width: w, height: h});
 
 svg.selectAll('circle').data(d3.range(10))
   .enter()
     .append('circle')
-    .attr('r', function(d) {return 20;})
-    .attr('cx', function(d) {
-      return Math.random() * w;
-    })
-    .attr('cy', function(d) {
-      return Math.random() * h;
-    })
+    .attr({r: radius, 
+          cx: function(d) {
+                return Math.random() * w;
+              },
+          cy: function(d) {
+                return Math.random() * h;
+              },
+          class: 'badCircle'
+          })
     .style('fill', function(d) {
       if(d % 2 === 0) {
         return "pink";
@@ -40,25 +43,17 @@ svg.selectAll('circle').data(d3.range(10))
         return "blue";
       }
     })
-    .attr('class', 'badCircle')
-    .style('fill', "url(#image)");
-
+    // .style('fill', "url(#image)")
+;
 
 var drag = d3.behavior.drag()
   .on("drag", function(d){
-    d.x += d3.event.dx;
-    d.y += d3.event.dy;
-    d3.select(this).attr("cx", function(d){
-      return d.x;
-    });
-    d3.select(this).attr("cy", function(d){
-      return d.y;
-    });
+    d3.select(this).attr( {cx: d3.event.x, cy: d3.event.y} );
   });
 
 svg.append('circle')
   .data([{x: 300, y: 300}])
-  .attr('r', function(d) {return 20;})
+  .attr({r: radius})
   .attr('cx', function(d) {
     return d.x;
   })
@@ -71,18 +66,25 @@ svg.append('circle')
   .attr('class', 'goodCircle')
   .call(drag);
 
+  var player = d3.select('.goodCircle');
+  var enemies = d3.selectAll('.badCircle');
 
 // code runs every 1 second
-setInterval( function () {
-  d3.selectAll('.badCircle').transition()
+var enemyMove = function () {
+  enemies.each( function () {
+    d3.select(this).transition()
     .duration(1000)
     .attr('cx', function(d) {
       return Math.random() * w;
     })
     .attr('cy', function(d) {
       return Math.random() * h;
-    });
-}, 1000);
+    }).delay(Math.random() * 3000)
+    .each("end", enemyMove);
+  });
+  //enemyMove();
+};
+enemyMove();
 
 var checkDistance = function(x1, y1, x2, y2) {
   return Math.sqrt((Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)));
@@ -90,19 +92,19 @@ var checkDistance = function(x1, y1, x2, y2) {
 
 
 var checkCollision = function() {
-  var player = d3.select('.goodCircle');
-  var enemies = d3.selectAll('.badCircle');
-  enemies.attr('cx', function(d) {
-    var enemyX = (d3.select(this).attr('cx'));
-    var enemyY = (d3.select(this).attr('cy'));
-    var enemyR = d3.select(this).attr('r');
-    var playerX = player.attr('cx');
-    var playerY = player.attr('cy');
-    var playerR = player.attr('r');
+  enemies.each(function(d) {
+    var enemyX = parseInt(d3.select(this).attr('cx'), 10);
+    var enemyY = parseInt(d3.select(this).attr('cy'), 10);
+    var enemyR = parseInt(d3.select(this).attr('r'), 10);
+    var playerX = parseInt(player.attr('cx'), 10);
+    var playerY = parseInt(player.attr('cy'), 10);
+    var playerR = parseInt(player.attr('r'), 10);
 
     d3.select('.current span').text(1 + +d3.select('.current span').text());
 
-    if(checkDistance(playerX, playerY, enemyX, enemyY) < (+enemyR + +playerR)) {
+    var distanceBetween = checkDistance(playerX, playerY, enemyX, enemyY);
+
+    if( distanceBetween < enemyR + playerR) {
       d3.select('.collisions span').text(1 + +d3.select('.collisions span').text());
       //set high score
       if ( +d3.select('.high span').text() < +d3.select('.current span').text() ) {
@@ -110,17 +112,10 @@ var checkCollision = function() {
       }
       //set score to zero
       d3.select('.current span').text(0);
-
-    } 
+    }
   });
 };
 
 // collision clock
-setInterval(function() {
-  checkCollision();
-}, 100
-);
-
-// score increment
-setInterval 
+d3.timer(checkCollision);
 
